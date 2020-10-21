192686846e6637da913b0e5fcd29d65271c4b566
Jeff T <jeff.thao92@gmail.com>
2020-10-20 23:15:22 -0500

    changed lib files - changed myName and other 'my' objects into just regular name, id, email, etc for intern.js, manager, employee, and engineer. renamed mainMenu function into createTeam to be more descriptive. Also made createManager function to prompt questions for managers.

--- app.js
+++ app.js
@@ -10,39 +10,34 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
 let employeeArray = []
 
-function mainMenu() {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is their name?",
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "What is their id?",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "What is their email?",
-    },
-    {
-      type: "list",
-      message: "What is their role?",
-      name: "role",
-      choices: [
-        "manager",
-        "engineer",
-        "intern"
-      ]
-      
-    }
-  ]);
-}
+function createTeam() {
 
-mainMenu();
-
+  function createManager() {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the manager's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the manager's id?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the manager's email?",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the manager's office phone number?",
+      },
+    ]).then(function (managerAnswers) {
+      const manager = new Manager(managerAnswers.name, managerAnswers.id, managerAnswers.email, managerAnswers.officeNumber);
+      employeeArray.push(manager);
+      createMoreMembers();
+    });
