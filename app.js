05515cbaf15d52151046631f577c7f11c1de202b
Jeff T <jeff.thao92@gmail.com>
2020-10-20 23:44:04 -0500

    added createMembers function to add more members, if manager then go to createManager, if engineer, then ask engineer questions, if intern, then ask intern questions.

--- app.js
+++ app.js
@@ -41,24 +41,101 @@ function createTeam() {
       employeeArray.push(manager);
       createMoreMembers();
     });
-    
-    function createMoreMembers() {
-      inquirer.prompt([
-        {
-          type: "list",
-          name: "create",
-          message: "Would you like to add more team members?",
-          choices: ["Yes", "No"]
-        }
-      ]).then(function (answers) {
-        if (answers.create === true) {
-          createMembers()
-        } else {
-          //
-        }
-      });
+  }
+
+  function createMoreMembers() {
+    inquirer.prompt([
+      {
+        type: "list",
+        name: "create",
+        message: "Would you like to add more team members?",
+        choices: ["Yes", "No"]
+      }
+    ]).then(function (answers) {
+      if (answers.create === true) {
+        createMembers()
+      } else {
+        createFinishedTeam()
+      }
+    });
+  }
+
+  function createMembers() {
+    inquirer.prompt([
+      {
+        type: "list",
+        name: "role",
+        message: "What is the team member's role?",
+        choices: ["Manager", "Engineer", "Intern"]
+      }
+    ]).then(function (answers) {
+      if (answers.role === "Manager") {
+        createManager();
+      } else if (answers.role === "Engineer") {
+        inquirer.prompt([
+          {
+            type: "input",
+            name: "name",
+            message: "What is the Engineer's name?",
+          },
+          {
+            type: "input",
+            name: "id",
+            message: "What is their id?",
+          },
+          {
+            type: "input",
+            name: "email",
+            message: "What is their email?",
+          },
+          {
+            type: "input",
+            name: "github",
+            message: "What is the Engineer's Github account?",
+          }
+        ]).then(function (engineerAnswers) {
+          const engineer = new Engineer(engineerAnswers.name, engineerAnswers.id, engineerAnswers.email, engineerAnswers.github);
+          employeeArray.push(manager);
+          createMoreMembers();
+        });
+      } else (answers.role === "Intern"){
+        inquirer.prompt([
+          {
+            type: "input",
+            name: "name",
+            message: "What is the Intern's name?",
+          },
+          {
+            type: "input",
+            name: "id",
+            message: "What is their id?",
+          },
+          {
+            type: "input",
+            name: "email",
+            message: "What is their email?",
+          },
+          {
+            type: "input",
+            name: "school",
+            message: "What is the Intern's school?",
+          }
+        ]).then(function (internAnswers) {
+          const intern = new Intern(internAnswers.name, internAnswers.id, internAnswers.email, internAnswers.school);
+          employeeArray.push(manager);
+          createMoreMembers();
+        });
+      }
     }
   }
+  
+      //   function createFinishedTeam() {
+      //   fs.readFile(__dirname + "./templates/main.html", function (err, data) {
+      //     if (err) throw err;
+      //   }
+      //   )
+      // }
+  
   createManager();
 }
-createTeam();
+  createTeam();
